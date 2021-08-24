66c7d5f50ecabc4e473474804f6b6dfd254b7f41
itskoi <19127037@student.hcmus.edu.vn>
2021-08-24 20:36:56 +0700

    Aug 24 2021

--- scientifit/lib/main.dart
+++ scientifit/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -8,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Text('Home'),
+    );
   }
 }
