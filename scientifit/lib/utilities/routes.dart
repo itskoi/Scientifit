606028e0bea6d69f0a0987586d44e4ca1ddecd68
itskoi <19127037@student.hcmus.edu.vn>
2021-08-26 12:06:41 +0700

    Aug 26 2021

--- scientifit/lib/utilities/routes.dart
+++ scientifit/lib/utilities/routes.dart
@@ -3,11 +3,13 @@ import 'package:scientifit/screens/authentication/register.dart';
 import 'package:scientifit/screens/authentication/signin.dart';
 import 'package:scientifit/screens/coordinator.dart';
 import 'package:scientifit/screens/diary/diaryhome.dart';
+import 'package:scientifit/screens/profile/profile.dart';
 
 final myRoutes = {
   '/': (context) => Coordinator(),
   '/auth': (context) => AuthMain(),
   '/signin': (context) => SignIn(),
   '/register': (context) => Register(),
-  '/diary': (context) => DiaryHome()
+  '/diary': (context) => DiaryHome(),
+  '/profile': (context) => Profile(),
 };
