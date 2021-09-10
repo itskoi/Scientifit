3c211461999fe89b363c5e7984fc8d3eeea25783
is.koi <sunshine.nbk@gmail.com>
2021-09-10 16:42:24 +0700

    Update new feature
    Sep 10, 2021
    - Add [Delete diary entries] by dragging it out the box.
    - Add removeExerciseFromDiary and removeFoodFromDiary.
    - Add try catch block to add/remove food/exercise functions in DB services.
    
    **BUG**: can not remove food/ex right after adding a new food/ex to diary

--- scientifit/lib/utilities/templates.dart
+++ scientifit/lib/utilities/templates.dart
@@ -5,8 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:scientifit/utilities/singleton.dart' as global;
 
 class ExerciseCard extends StatefulWidget {
-
-  ExerciseCard( { required this.exercise });
+  ExerciseCard({required this.exercise});
 
   final ExerciseEntry exercise;
 
@@ -17,74 +16,142 @@ class ExerciseCard extends StatefulWidget {
 class _ExerciseCardState extends State<ExerciseCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'EXERCISE',
-                style: TextStyle(
-                    color: Color(0xFF2B463C),
-                    fontFamily: 'OpenSans',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18
-                ),
-              ),
-              Container(
-                height: 60,
-                child: Text(
-                  global.dbExercise[widget.exercise.did]!.name,
-                  overflow: TextOverflow.fade,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'OpenSans',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Draggable<ExerciseEntry>(
+      onDraggableCanceled: (_, __) async {
+        await global.databaseService!.removeExerciseFromDiary(widget.exercise);
+        print('Remove exercise: ${global.dbExercise[widget.exercise.did]!.name}!');
+      },
+      data: widget.exercise,
+      feedback: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.white.withAlpha(200),
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    widget.exercise.caloriesBurned.toString(),
+                    'EXERCISE',
                     style: TextStyle(
-                        color: Color(0xFFDD8600),
+                        color: Color(0xFF2B463C),
                         fontFamily: 'OpenSans',
                         fontWeight: FontWeight.w700,
-                        fontSize: 18
+                        fontSize: 18),
+                  ),
+                  Container(
+                    height: 60,
+                    child: Text(
+                      global.dbExercise[widget.exercise.did]!.name,
+                      overflow: TextOverflow.fade,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20),
                     ),
                   ),
-                  SizedBox(width: 5,),
-                  Text(
-                    global.dbExercise[widget.exercise.did]!.caloriesUnit,
-                    style: TextStyle(
-                        color: Color(0xFFF9B751),
-                        fontFamily: 'OpenSans',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.exercise.caloriesBurned.toString(),
+                        style: TextStyle(
+                            color: Color(0xFFDD8600),
+                            fontFamily: 'OpenSans',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        global.dbExercise[widget.exercise.did]!.caloriesUnit,
+                        style: TextStyle(
+                            color: Color(0xFFF9B751),
+                            fontFamily: 'OpenSans',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
+          )),
+      child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-        )
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'EXERCISE',
+                  style: TextStyle(
+                      color: Color(0xFF2B463C),
+                      fontFamily: 'OpenSans',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18),
+                ),
+                Container(
+                  height: 60,
+                  child: Text(
+                    global.dbExercise[widget.exercise.did]!.name,
+                    overflow: TextOverflow.fade,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'OpenSans',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.exercise.caloriesBurned.toString(),
+                      style: TextStyle(
+                          color: Color(0xFFDD8600),
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      global.dbExercise[widget.exercise.did]!.caloriesUnit,
+                      style: TextStyle(
+                          color: Color(0xFFF9B751),
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
-class FoodCard extends StatefulWidget {
 
-  FoodCard( { required this.food });
+class FoodCard extends StatefulWidget {
+  FoodCard({required this.food});
 
   final FoodEntry food;
 
@@ -95,68 +162,137 @@ class FoodCard extends StatefulWidget {
 class _FoodCardState extends State<FoodCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'FOOD',
-                style: TextStyle(
-                    color: Color(0xFFBC2057),
-                    fontFamily: 'OpenSans',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 18
-                ),
-              ),
-              Container(
-                height: 70,
-                child: Text(
-                  global.dbFood[widget.food.did]!.name,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.fade,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'OpenSans',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Draggable<FoodEntry>(
+      onDraggableCanceled: (_, __) async {
+        await global.databaseService!.removeFoodFromDiary(widget.food);
+        print('Remove food: ${global.dbFood[widget.food.did]!.name}!');
+      },
+      data: widget.food,
+      feedback: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.white.withAlpha(200),
+          child: SizedBox(
+            height: 150,
+            width: 150,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    widget.food.caloriesGained.toString(),
+                    'FOOD',
                     style: TextStyle(
-                        color: Color(0xFFDD8600),
+                        color: Color(0xFFBC2057),
                         fontFamily: 'OpenSans',
                         fontWeight: FontWeight.w700,
-                        fontSize: 18
+                        fontSize: 18),
+                  ),
+                  Container(
+                    height: 70,
+                    child: Text(
+                      global.dbFood[widget.food.did]!.name,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20),
                     ),
                   ),
-                  SizedBox(width: 5,),
-                  Text(
-                    global.dbFood[widget.food.did]!.caloriesUnit,
-                    style: TextStyle(
-                        color: Color(0xFFF9B751),
-                        fontFamily: 'OpenSans',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.food.caloriesGained.toString(),
+                        style: TextStyle(
+                            color: Color(0xFFDD8600),
+                            fontFamily: 'OpenSans',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        global.dbFood[widget.food.did]!.caloriesUnit,
+                        style: TextStyle(
+                            color: Color(0xFFF9B751),
+                            fontFamily: 'OpenSans',
+                            fontWeight: FontWeight.w400,
+                            fontSize: 18),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        )
+      ),
+      child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'FOOD',
+                  style: TextStyle(
+                      color: Color(0xFFBC2057),
+                      fontFamily: 'OpenSans',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 18),
+                ),
+                Container(
+                  height: 70,
+                  child: Text(
+                    global.dbFood[widget.food.did]!.name,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.fade,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'OpenSans',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.food.caloriesGained.toString(),
+                      style: TextStyle(
+                          color: Color(0xFFDD8600),
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 18),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      global.dbFood[widget.food.did]!.caloriesUnit,
+                      style: TextStyle(
+                          color: Color(0xFFF9B751),
+                          fontFamily: 'OpenSans',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
